9c7a2769c55b06d09bc8d71deb95c3596b2a780a
rayshen <rayshen1012@gmail.com>
2015-07-08 18:29:49 +0800

    Initial Commit

---
+++ Lock2015/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lock2015
+//
+//  Created by shen on 15/7/8.
+//  Copyright (c) 2015年 shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Lock2015Tests/Lock2015Tests.m
@@ -0,0 +1,40 @@
+//
+//  Lock2015Tests.m
+//  Lock2015Tests
+//
+//  Created by shen on 15/7/8.
+//  Copyright (c) 2015年 shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Lock2015Tests : XCTestCase
+
+@end
+
+@implementation Lock2015Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
